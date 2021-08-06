ad2ecef357a3e9d714cec34c0af2cecbfc503ae1
cesar.arellano <cesar.arellano@gtec.com.mx>
2021-08-05 19:00:06 -0500

    Adding try and catch in getGifs function

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,16 +1,22 @@
 export const getGifs = async ( category ) => {
-  console.log(process.env);
   const apiKey = process.env.REACT_APP_GIFS_API_KEY;
-  console.log(apiKey);
-  const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=${ apiKey }`;
-  const resp = await fetch(url);
-  const { data } = await resp.json();
-  const gifs = data.map(( gif ) => {
-    return {
-      id: gif.id,
-      title: gif.title,
-      url: gif.images?.downsized.url,
-    }
-  });
-  return gifs;
+  try {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=${ apiKey }`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const gifs = (!data)
+      ? []  
+      : data.map(( gif ) => {
+        return {
+          id: gif.id,
+          title: gif.title,
+          url: gif.images?.downsized.url,
+        }
+      });
+    return gifs;
+
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 };
